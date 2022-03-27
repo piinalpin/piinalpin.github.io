cb6a557f25844acee069bc392aef66b1ce1804e7
Alvinditya Saputra <calvinjoe782@gmail.com>
2022-03-27 14:20:46 +0700

    update workflow

--- sync.sh
+++ sync.sh
@@ -1,1 +1,3 @@
+#/bin/bash
+echo "APP_ID: $ALGOLIA_APPLICATION_ID"
 curl -X POST -H "X-Algolia-API-Key: $ALGOLIA_API_KEY" -H "X-Algolia-Application-Id: $ALGOLIA_APPLICATION_ID" "https://$ALGOLIA_APPLICATION_ID.algolia.net/1/indexes/test-index/clear"
